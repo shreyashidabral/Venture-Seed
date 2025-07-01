7c1bae35b2659deb8c485673864fd614a6060840
Shreyashi Dabral <shreyashidabral@gmail.com>
2025-07-02 01:27:19 +0530

    Setup app, file & folder structure, routing and auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default page;
